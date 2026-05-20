92e80b8a5fdfaa5e8184befca01acf3dc154d3ca
susanjacke24-dev <susanjacke.24@gmail.com>
2026-05-20 17:28:08 -0600

    feat: add scheduled veterinary reporting and notification automation

--- routes/console.php
+++ routes/console.php
@@ -11,8 +11,8 @@ use Illuminate\Support\Facades\Schedule;
 
 // Registro del comando de automatización en el Scheduler
 // Se ejecuta a las 08:00 AM para la agenda y a las 06:30 PM para el resumen del día
-Schedule::command('mail:test')->dailyAt('08:00');
-Schedule::command('mail:test')->dailyAt('18:30');
+Schedule::command('puppycare:automate')->dailyAt('08:00');
+Schedule::command('puppycare:automate')->dailyAt('18:30');
 
 Artisan::command('inspire', function () {
     $this->comment(\Illuminate\Foundation\Inspiring::quote());
